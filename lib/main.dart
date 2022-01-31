49be84c7a184d2666b339beb78d22285d8ad46c0
Syntax007 <Syntax007@users.noreply.github.com>
2022-01-31 10:55:34 +0100

    validation and storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:flutter_getx/screens/first_screen.dart';
 import 'package:flutter_getx/screens/home_screen.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await GetStorage.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+   MyApp({Key? key}) : super(key: key);
+
+  var emailEditingController = TextEditingController();
+  var storage = GetStorage();
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/home",
-      getPages: [
-        GetPage(name: '/home', page: () => const HomeScreen()),
-        GetPage(name: '/first/:someValue', page: () => const FirstScreen())
-      ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: emailEditingController,
+                ),
+              ),
+              const SizedBox(height: 8,),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  child: const Text("Write"),
+                  onPressed: (){
+                    if (GetUtils.isEmail(emailEditingController.text)) {
+                      storage.write("email",
+                      emailEditingController.text);
+                    } else {
+                      Get.snackbar("InCorrect Email", "Privide Email in valid format",
+                      colorText: Colors.white,
+                      backgroundColor: Colors.red,
+                      snackPosition: SnackPosition.BOTTOM);
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(height: 8,),
+              ElevatedButton(
+                child: const Text("Read"),
+                onPressed: (){
+                  debugPrint("The Email is${storage.read("email")}");
+                } )
+
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
